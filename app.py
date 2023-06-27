e6fd315b2be35eb5d15c1e2612149e6fe52a9dbf
Anirruth <anirruthbun@gmail.com>
2023-06-27 11:07:14 +0530

    Final

---
+++ app.py
@@ -0,0 +1,66 @@
+from flask import Flask, render_template, request, redirect, url_for
+from flask_sqlalchemy import SQLAlchemy
+from Url_shortener.hash import calculateHash
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///C:/Users/Anirruth/Desktop/Url/Url_shortener/Url.db'
+app.config['SQLALCHEMY_TRACK_MODIFCATIONS'] = False
+
+db = SQLAlchemy(app)
+# used for first request
+initialized = False
+
+
+class Url(db.Model):
+    id_ = db.Column("id_", db.Integer, primary_key=True)
+    longurl = db.Column("longurl", db.String())
+    shorturl = db.Column("shorturl", db.String())
+
+    def __init__(self, longurl, shorturl):
+        self.longurl = longurl
+        self.shorturl = shorturl
+
+
+@app.before_request
+def initialize():
+    global initialized
+    if not initialized:
+        # Code to be executed before the first request
+        db.create_all()
+        initialized = True
+
+
+@app.route('/', methods=['POST', 'GET'])
+def home():
+    if request.method == "POST":
+        longurlinput = request.form["url_inp"]
+        urlexist = Url.query.filter_by(longurl=longurlinput).first()
+        if urlexist:
+            return redirect(url_for("display_short_url", urlinput=urlexist.shorturl))
+        else:
+            shorturlinput = calculateHash(longurlinput)
+            new_input_url = Url(longurlinput, shorturlinput)
+            db.session.add(new_input_url)
+            db.session.commit()
+            return redirect(url_for("display_short_url", urlinput=shorturlinput))
+    else:
+        return render_template('home.html')
+
+
+@app.route('/display/<urlinput>')
+def display_short_url(urlinput):
+    return render_template('shorturl.html', shorturlvalue=urlinput)
+
+
+@app.route('/<short_url>')
+def redirection(short_url):
+    longurlvalue = Url.query.filter_by(shorturl=short_url).first()
+    if longurlvalue:
+        return redirect(longurlvalue.longurl)
+    else:
+        return f'<h1>Url doesnt exist</h1>'
+
+
+if(__name__ == '__main__'):
+    app.run(port=5000, debug=True)
---
+++ hash.py
@@ -0,0 +1,99 @@
+import random
+import math
+sample = "YhdZsmnaxVlWQrzcPpIbBqXDvJuRStgyfiLeOCNHkFwEMTGjKLUoAZYxvncrI"
+
+
+def calculateHash(string):
+    baseval = ''
+    answer = []  # stores all index values
+
+    for ch in string:
+        tempval = ord(ch)
+        baseval += str(tempval)
+
+    # getting the base number value og the url string
+    basenum = int(baseval)
+    r1 = random.randint(51, 100)
+    #print("Baseval", basenum)
+    # calculating base index
+    basenum = basenum//r1
+    # print("Basenum", basenum)
+    # print()
+    index0 = basenum % 52
+    answer.append(index0)
+
+    # calculating first index
+    tempstr1 = str(basenum)
+    r2 = random.randint(121, 321)  # second random value
+    firstval = ''
+    for number in tempstr1:
+        tempval = ord(number)
+        tempval = tempval ^ r2
+        firstval += str(tempval)
+    firstintval = int(firstval)
+    # print("firstval", firstintval)
+    # print()
+    index1 = firstintval % 52
+    answer.append(index1)
+
+    # calculating second index
+    r3 = random.randint(2, 69)
+    secondintval = firstintval//r3
+    # print("Second", secondintval)
+    # print()
+    index2 = secondintval % 52
+    answer.append(index2)
+
+    # calculating third index
+    r4 = random.randint(1, 17)
+    secondintval = secondintval*r4
+    thirdval = ''
+    for ch in str(secondintval):
+        temp = ord(ch)
+        temp = temp*r4
+        thirdval += str(temp)
+    thirdintval = int(thirdval)//(9999999999*999999999)
+    # print("Third", thirdintval)
+    # print()
+    index3 = (thirdintval//r4) % 52
+    answer.append(index3)
+
+    # calculating fourth index
+    r5 = random.randint(43, 890)
+    thirdintval = thirdintval % r5
+    fourthval = ''
+    d = 7
+    m = 3
+    for ch in str(thirdintval):
+        temp = ord(ch)
+        temp = temp ^ r5
+        temp = (temp*m)//d
+        d += 2
+        m += 1
+        fourthval += str(temp)
+    fourthintval = int(fourthval)
+    # print("Fourth", fourthintval)
+    # print()
+    index4 = fourthintval % 52
+    answer.append(index4)
+
+    # calculating last index
+    r6 = random.randint(69, 199)
+    r7 = random.randint(89, 132)
+    lastintval = fourthintval*r6*r7
+    lastintval = lastintval ^ r7
+    lastintval = int(math.sqrt(lastintval))
+    # print("Last", lastintval)
+    # print()
+    indexlast = lastintval % 52
+    answer.append(indexlast)
+
+    hashval = ""
+    for i in answer:
+        hashval += sample[i]
+    # print(hashval)
+
+    return(hashval)
+
+
+print(calculateHash("https://www.analyticsinsight.net/top-10-vs-code-extensions-for-python-developers-to-learn-in-2023/"))
